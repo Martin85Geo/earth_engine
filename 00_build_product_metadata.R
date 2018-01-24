0a1f5698a6c66f491001930c2e3d72de6aae85cc
dccasey <daniel.c.casey@gmail.com>
2018-01-24 02:45:44 -0500

    build out qa functionality and have python script save the dates of the files

--- 00_build_product_metadata.R
+++ 00_build_product_metadata.R
@@ -7,7 +7,8 @@ lst = setDT(expand.grid(product =c('MOD11A2','MYD11A2'),
                    lower = 7500,
                    upper = 65535,
                    scale = .02,
-                   sensor = 'MODIS'))
+                   sensor = 'MODIS',
+                   qa_bits = 8))
 lst[, qa_layer := paste0('QC_', tstrsplit(variables, "_")[[2]])]
 
 #NDVI and EVI from MOD13A1 and MYD13A1
@@ -18,6 +19,7 @@ vis = setDT(expand.grid(product = c('MOD13A1','MYD13A1'),
                         upper = 10000,
                         scale = .0001,
                         qa_layer = 'DetailedQA',
+                        qa_bits = 16,
                         sensor = 'modis'))
 
 #night time lights
@@ -25,4 +27,6 @@ ntl = setDT(expand.grid(product = 'DMSPNTL',
                         version = 4,
                         sensor = 'DMSPNTL',
                         gee_collection_id = 'NOAA/DMSP-OLS/CALIBRATED_LIGHTS_V4'))
+
+
 
