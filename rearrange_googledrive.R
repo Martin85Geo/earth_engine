e95e4e16ed64a1d6690eb896a62bcedad2f2f6ac
dccasey <daniel.c.casey@gmail.com>
2018-04-09 01:16:54 -0400

    update directory structure and use R to interface with googledrive

---
+++ rearrange_googledrive.R
@@ -0,0 +1,45 @@
+library('googledrive')
+library('data.table')
+#a list of all files in modis
+modis_files = drive_ls(path = 'modis')
+
+#reorganize files into the following path
+#earth_engine/product_name/city
+mods = as.data.table(modis_files[,1:2])
+mods[, c('city', 'product_name'):=tstrsplit(name,split = '_', fixed = T, keep = c(1,2))]
+mods[, variable:= substr(name, nchar(paste(city,product_name))+2, nchar(name) - 9)]
+
+#some of the surface reflectance is weird and has addition _[0-9] as a suffix. Remove these for now
+weird_files = mods[grepl('sur_refl', variable, fixed = T) & substr(variable, nchar(variable)-1,nchar(variable)-1)=='_',]
+
+mods = mods[!id %in% weird_files[,id]]
+mods[,new_path:=paste0('earth_engine/',product_name,'/', city,'/')]
+
+
+#create the new folder structure
+# existing_folder = drive_ls('~/earth_engine', type = 'folder', recursive = T)
+# for(pn in unique(mods[,product_name])){
+#   
+#   drive_mkdir(paste0('~/earth_engine/',pn,'/'))
+#   
+#   for(cit in unique(mods[,city])){
+#     drive_mkdir(paste0('~/earth_engine/',pn,'/',cit,'/'))
+#   }
+# }
+
+#Move the things
+iter = 1
+for(i in 1:nrow(mods)){
+  drive_mv(as_id(mods[i,id]), mods[i,new_path], verbose = F)
+  iter = iter + 1
+  
+  if(iter %% 9 == 0){
+    Sys.sleep(1.1)
+  }
+  
+  if(iter %% 100 == 0){
+    print(iter)
+  }
+  
+}
+
---
+++ sync_gdrive.R
@@ -0,0 +1,37 @@
+library('googledrive')
+
+overwrite_files = F
+base_path = '/media/dan/earth_engine/'
+#compare google drive vs. on disk
+drive_folders = drive_ls(path = '~/earth_engine/')
+
+#for each folder
+for(i in 1:nrow(drive_folders)){
+  #get files from drive
+  fff = drive_ls(paste0('~/earth_engine/', drive_folders[i,'name']), pattern = '\\.tif',recursive = T)
+  #get files from disk
+  ondisk = basename(list.files(path = paste0(base_path, drive_folders[i,'name']), recursive = T))
+  
+  #subset out files already downloaded
+  if(!overwrite_files){
+    fff = fff[!fff$name %in% ondisk,]
+  }
+  
+  #check if the image already exists, if not download
+  if(nrow(fff)>0){
+    for(img in 1:nrow(fff)){
+      
+      #get folder path
+      city = strsplit(fff$name[img],split = '_',fixed = T)[[1]][1]
+      ootpoot = file.path(base_path,drive_folders$name[i], city)
+      dir.create(ootpoot, recursive = T)
+      
+      drive_download(as_id(fff$id[img]), path = paste0(ootpoot, '/',fff$name[img]), overwrite = T)
+      
+      #Sys.sleep(1)
+    }
+  }
+  
+}
+
+disk_files = list.files(path = '/media/dan/earth_engine/',recursive = T)
